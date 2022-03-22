43a1d03482439afeb97533f0279d1ffc781ef714
SAMUEL_SERRA_ALVES <22101497@ALFAF42.FLORESTA>
2022-03-22 15:00:55 -0300

    'details'

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.use(express.static("public"));
 app.set('view engine', 'ejs');
 
 app.get("/", (req, res) => {
-    res.render("index")
+    res.render("home")
 });
 
 app.get("/details", (req, res) => {
